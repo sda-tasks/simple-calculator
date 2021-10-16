929bef0665f968cfd1e431414e2f0264a5078b88
Adam Witowski <adwitos@gmail.com>
2021-10-16 15:13:09 +0200

    page 0 exercise 7

---
+++ src/main/java/pl/sda/calculator/Calculator.java
@@ -0,0 +1,31 @@
+package pl.sda.calculator;
+
+public class Calculator {
+    public static final String PLUS = "+";
+    public static final String MINUS = "-";
+    public static final String MULTIPLY = "*";
+    public static final String DIVIDE = "/";
+
+    public double calculate(double a, double b, String operator) {
+        double result = 0;
+
+        switch (operator) {
+            case PLUS:
+                result = a + b;
+                break;
+            case MINUS:
+                result = a - b;
+                break;
+            case MULTIPLY:
+                result = a * b;
+                break;
+            case DIVIDE:
+                result = a / b;
+                break;
+            default:
+                System.out.println("Podany operator nie istnieje!");
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/pl/sda/calculator/CalculatorLauncher.java
@@ -0,0 +1,22 @@
+package pl.sda.calculator;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CalculatorLauncher {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.US);
+
+        double a = input.nextDouble();
+        input.nextLine();
+        String operator = input.nextLine();
+        double b = input.nextDouble();
+
+        Calculator calc = new Calculator();
+        double result = calc.calculate(a, b, operator);
+        System.out.println(a + operator + b + " = " + result);
+
+        input.close();
+    }
+}
